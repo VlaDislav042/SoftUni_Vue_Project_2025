c46a0c1edacfb07473609aef724b2ccda5a9f4cf
Vladislav Georgiev <vlafi90@gmail.com>
2024-12-12 15:34:47 +0200

    Installing the demo version
    Installing the demo version and splitting it up on parts

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <script>
+import AppFooter from './components/AppFooter.vue';
+import AppHeader from './components/AppHeader.vue';
 import ComponentsStarter from './components/ComponentsStarter.vue';
 
 export default {
   components: {
     ComponentsStarter,
+    AppFooter,
+    AppHeader,
   },
 };
 </script>
 
 <template>
-  <ComponentsStarter />
+  <AppHeader />
+  <main>
+    <ComponentsStarter />
+  </main>
+  <AppFooter />
 </template>
---
+++ src/pages/Product/components/Categories.vue
@@ -0,0 +1,39 @@
+<template>
+    <ul role="list" class="categories">
+        <li>
+            <button class="primary">
+                Women's Dresses
+            </button>
+        </li>
+        <li>
+            <button class="secondary outline">
+                Women's Shoes
+            </button>
+        </li>
+        <li>
+            <button class="secondary outline">
+                Men's Shirts
+            </button>
+        </li>
+        <li>
+            <button class="secondary outline">
+                Men's Shoes
+            </button>
+        </li>
+        <li>
+            <button class="secondary outline">
+                Men's Watches
+            </button>
+        </li>
+        <li>
+            <button class="secondary outline">
+                Women's Watches
+            </button>
+        </li>
+        <li>
+            <button class="secondary outline">
+                Sunglasses
+            </button>
+        </li>
+    </ul>
+</template>
